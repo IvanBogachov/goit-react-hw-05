3365e828d0bee3edc6cea16c95b0529fc61851ed
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2024-10-20 17:56:34 +0300

    fix error

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const IMG_URL = "https://image.tmdb.org/t/p/w500";
-export const DEFAULT_IMG_URL = "/public/no-image_s.jpg";
+export const DEFAULT_IMG_URL = "https://i.imgur.com/WfuAcz1.png?2";
 export const DEFAULT_DATE = "0000-00-00";
 export const DEFAULT_TITLE = "Title";
 export const DEFAULT_LINK = "/";
